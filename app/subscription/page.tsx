84d95c471d39aff333179bb99cfe75d5e28d7125
Lilllian Rocha <lillianrocha.negocio@gmail.com>
2025-11-28 17:16:52 -0300

    chore: add prisma and database setup

---
+++ app/subscription/page.tsx
@@ -0,0 +1,5 @@
+const Subscription = () => {
+  return <h1>Welcome to the Subscription Page</h1>;
+};
+
+export default Subscription;
---
+++ app/transactions/page.tsx
@@ -0,0 +1,5 @@
+const Transactions = () => {
+  return <h1>Welcome to the Transactions Page</h1>;
+};
+
+export default Transactions;
