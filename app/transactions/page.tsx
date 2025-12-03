c2ab727868d28f9e7588fe01387143c3158cc3b2
Lilllian Rocha <lillianrocha.negocio@gmail.com>
2025-12-03 10:54:49 -0300

    feat: add transaction type badge

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,5 +1,24 @@
-const TransactionsPage = () => {
-  return <h1>Welcome to the Transactions Page</h1>;
-};
+import { ArrowDownUp, ArrowDownUpIcon } from "lucide-react";
+import { Button } from "../_components/ui/button";
+import { db } from "../_lib/prisma";
+import { DataTable } from "../_components/ui/data-table";
+import { TransactionColumns } from "./_columns";
 
+const TransactionsPage = async () => {
+  const transactions = await db.transaction.findMany({});
+
+  return (
+    <div className="p-6 space-y-6">
+      <div className="flex w-full justify-between items-center">
+        <h1 className="text-2xl font-bold">Transações</h1>
+        <Button className="rounded-full">          
+          Adicionar transação
+          <ArrowDownUpIcon/>
+        </Button>
+      </div>
+      <DataTable columns={TransactionColumns} data={transactions} />
+    </div>
+  );
+
+}
 export default TransactionsPage;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,7 +10,8 @@ const config: Config = {
   theme: {
   	extend: {
   		colors: {
-  			background: 'hsl(var(--background))',
+  			danger: "#F6352E",
+			background: 'hsl(var(--background))',
   			foreground: 'hsl(var(--foreground))',
   			card: {
   				DEFAULT: 'hsl(var(--card))',
