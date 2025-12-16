6f4ac183894c9e1a234ae15e8c65f596def2145f
Lilllian Rocha <lillianrocha.negocio@gmail.com>
2025-12-16 18:29:27 -0300

    feat: add transactions pie chart component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -4,6 +4,10 @@ import Navbar from "../_components/navbar";
 import SummaryCards from "./_components/summary-cards";
 import TimeSelect from "./_components/time-select";
 import { isMatch } from "date-fns";
+import TransactionsPieChart from "./_components/transactions-pie-chart";
+import { getDashboard } from "../_data/get-dashboard";
+import ExpensesPerCategory from "./_components/expenses-per-category";
+import LastTransactions from "./_components/last-transactions";
 
 interface HomeProps {
   searchParams: {
@@ -12,18 +16,15 @@ interface HomeProps {
 }
 
 const Home = async ({ searchParams: { month } }: HomeProps) => {
-  const {userId} = await auth();
+  const { userId } = await auth();
   if (!userId) {
     redirect("/login");
-  } 
-
+  }
   const monthIsInvalid = !month || !isMatch(month, "MM");
   if (monthIsInvalid) {
     redirect(`?month=${new Date().getMonth() + 1}`);
   }
-
   const dashboard = await getDashboard(month);
-  
   return (
     <>
       <Navbar />
@@ -32,11 +33,21 @@ const Home = async ({ searchParams: { month } }: HomeProps) => {
           <h1 className="text-2xl font-bold">Dashboard</h1>
           <TimeSelect />
         </div>
-        <SummaryCards />
+        <div className="grid h-full grid-cols-[2fr,1fr] gap-6 overflow-hidden">
+          <div className="flex flex-col gap-6 overflow-hidden">
+            <SummaryCards month={month} {...dashboard} />
+            <div className="grid h-full grid-cols-3 grid-rows-1 gap-6 overflow-hidden">
+              <TransactionsPieChart {...dashboard} />
+              <ExpensesPerCategory
+                expensesPerCategory={dashboard.totalExpensePerCategory}
+              />
+            </div>
+          </div>
+          <LastTransactions lastTransactions={dashboard.lastTransactions} />
+        </div>
       </div>
-      
-    </>    
-  )
+    </>
+  );
 };
 
 export default Home;
