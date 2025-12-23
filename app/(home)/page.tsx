360700e403fad57d2d8d1dc5e4ecaea08a6b2cd5
Lilllian Rocha <lillianrocha.negocio@gmail.com>
2025-12-23 17:09:32 -0300

    feat: ensure use can not create transaction on free plan if above limit

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,4 +1,4 @@
-import { auth } from "@clerk/nextjs/server";
+import { auth, clerkClient } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import Navbar from "../_components/navbar";
 import SummaryCards from "./_components/summary-cards";
@@ -8,6 +8,8 @@ import TransactionsPieChart from "./_components/transactions-pie-chart";
 import { getDashboard } from "../_data/get-dashboard";
 import ExpensesPerCategory from "./_components/expenses-per-category";
 import LastTransactions from "./_components/last-transactions";
+import { canUserAddTransaction } from "../_data/can-user-add-transaction";
+//import AiReportButton from "./_components/ai-report-button";
 
 interface HomeProps {
   searchParams: {
@@ -25,17 +27,23 @@ const Home = async ({ searchParams: { month } }: HomeProps) => {
     redirect(`?month=${new Date().getMonth() + 1}`);
   }
   const dashboard = await getDashboard(month);
+  const userCanAddTransaction = await canUserAddTransaction();
+  const user = await clerkClient().users.getUser(userId);
   return (
     <>
       <Navbar />
       <div className="flex h-full flex-col space-y-6 overflow-hidden p-6">
         <div className="flex justify-between">
           <h1 className="text-2xl font-bold">Dashboard</h1>
-          <TimeSelect />
+          
         </div>
         <div className="grid h-full grid-cols-[2fr,1fr] gap-6 overflow-hidden">
           <div className="flex flex-col gap-6 overflow-hidden">
-            <SummaryCards month={month} {...dashboard} />
+            <SummaryCards
+              month={month}
+              {...dashboard}
+              userCanAddTransaction={userCanAddTransaction}
+            />
             <div className="grid h-full grid-cols-3 grid-rows-1 gap-6 overflow-hidden">
               <TransactionsPieChart {...dashboard} />
               <ExpensesPerCategory
--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -5,32 +5,35 @@ import AddTransactionButton from "../_components/add-transaction-button";
 import Navbar from "../_components/navbar";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
+import { ScrollArea } from "../_components/ui/scroll-area";
+import { canUserAddTransaction } from "../_data/can-user-add-transaction";
 
 const TransactionsPage = async () => {
   const { userId } = await auth();
   if (!userId) {
     redirect("/login");
   }
-  
   const transactions = await db.transaction.findMany({
     where: {
       userId,
     },
   });
-
+  const userCanAddTransaction = await canUserAddTransaction();
   return (
     <>
       <Navbar />
-      <div className="p-6 space-y-6">
-        <div className="flex w-full justify-between items-center">
+      <div className="space-y-6 overflow-hidden p-6">
+        {/* TÍTULO E BOTÃO */}
+        <div className="flex w-full items-center justify-between">
           <h1 className="text-2xl font-bold">Transações</h1>
-          <AddTransactionButton />
+          <AddTransactionButton userCanAddTransaction={userCanAddTransaction} />
         </div>
-        <DataTable columns={TransactionColumns} data={transactions} />
+        <ScrollArea>
+          <DataTable columns={TransactionColumns} data={transactions} />
+        </ScrollArea>
       </div>
     </>
-    
   );
+};
 
-}
 export default TransactionsPage;
